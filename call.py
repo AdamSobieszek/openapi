ca618bf662baa766bce3b7736eda165c1a996849
AdamSobieszek <74745693+AdamSobieszek@users.noreply.github.com>
2023-07-14 10:23:56 +0200

    Update call.py

--- call.py
+++ call.py
@@ -266,6 +266,7 @@ class File:
                         for v in values_unordered:
                             if line["input"] == v[0]["input"]:
                                 self.values.append(v)
+                                continue
                 except:
                     print("Could not order")
 
